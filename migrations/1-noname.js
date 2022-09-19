e33d0b151b01f1930d02644190cc37ba708ae388
elduke717 <njhuemmer@gmail.com>
2022-09-19 17:13:43 -0400

    updated packages for deployment, deploy from Linux laptop

---
+++ migrations/1-noname.js
@@ -0,0 +1,199 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * createTable "Users", deps: []
+ * createTable "Courses", deps: [Users]
+ *
+ **/
+
+var info = {
+    "revision": 1,
+    "name": "noname",
+    "created": "2022-09-19T21:11:40.953Z",
+    "comment": ""
+};
+
+var migrationCommands = [{
+        fn: "createTable",
+        params: [
+            "Users",
+            {
+                "id": {
+                    "type": Sequelize.INTEGER,
+                    "field": "id",
+                    "autoIncrement": true,
+                    "primaryKey": true,
+                    "allowNull": false
+                },
+                "firstName": {
+                    "type": Sequelize.STRING,
+                    "field": "firstName",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A first name is required"
+                        },
+                        "notEmpty": {
+                            "msg": "Please provide a first name"
+                        }
+                    },
+                    "allowNull": false
+                },
+                "lastName": {
+                    "type": Sequelize.STRING,
+                    "field": "lastName",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A last name is required"
+                        },
+                        "notEmpty": {
+                            "msg": "Please provide a last name"
+                        }
+                    },
+                    "allowNull": false
+                },
+                "emailAddress": {
+                    "type": Sequelize.STRING,
+                    "field": "emailAddress",
+                    "validate": {
+                        "notNull": {
+                            "msg": "An e-mail is required"
+                        },
+                        "isEmail": {
+                            "msg": "Please provide a valid email address"
+                        }
+                    },
+                    "unique": {
+                        "msg": "The email you entered already exists"
+                    },
+                    "allowNull": false
+                },
+                "password": {
+                    "type": Sequelize.STRING,
+                    "field": "password",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A password is required"
+                        },
+                        "notEmpty": {
+                            "msg": "Please provide a password"
+                        },
+                        "len": {
+                            "args": [8, 20],
+                            "msg": "The password must be between 8 and 20 characters in length"
+                        }
+                    },
+                    "allowNull": false
+                },
+                "createdAt": {
+                    "type": Sequelize.DATE,
+                    "field": "createdAt",
+                    "allowNull": false
+                },
+                "updatedAt": {
+                    "type": Sequelize.DATE,
+                    "field": "updatedAt",
+                    "allowNull": false
+                }
+            },
+            {}
+        ]
+    },
+    {
+        fn: "createTable",
+        params: [
+            "Courses",
+            {
+                "id": {
+                    "type": Sequelize.INTEGER,
+                    "field": "id",
+                    "autoIncrement": true,
+                    "primaryKey": true,
+                    "allowNull": false
+                },
+                "title": {
+                    "type": Sequelize.STRING,
+                    "field": "title",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A course name is required."
+                        },
+                        "notEmpty": {
+                            "msg": "Please provide a course name"
+                        }
+                    },
+                    "allowNull": false
+                },
+                "description": {
+                    "type": Sequelize.TEXT,
+                    "field": "description",
+                    "validate": {
+                        "notNull": {
+                            "msg": "A description is required"
+                        },
+                        "notEmpty": {
+                            "msg": "Please enter a brief description for the course"
+                        }
+                    },
+                    "allowNull": false
+                },
+                "estimatedTime": {
+                    "type": Sequelize.STRING,
+                    "field": "estimatedTime"
+                },
+                "materialsNeeded": {
+                    "type": Sequelize.STRING,
+                    "field": "materialsNeeded"
+                },
+                "createdAt": {
+                    "type": Sequelize.DATE,
+                    "field": "createdAt",
+                    "allowNull": false
+                },
+                "updatedAt": {
+                    "type": Sequelize.DATE,
+                    "field": "updatedAt",
+                    "allowNull": false
+                },
+                "userId": {
+                    "type": Sequelize.INTEGER,
+                    "field": "userId",
+                    "onUpdate": "CASCADE",
+                    "onDelete": "NO ACTION",
+                    "references": {
+                        "model": "Users",
+                        "key": "id"
+                    },
+                    "allowNull": false
+                }
+            },
+            {}
+        ]
+    }
+];
+
+module.exports = {
+    pos: 0,
+    up: function(queryInterface, Sequelize)
+    {
+        var index = this.pos;
+        return new Promise(function(resolve, reject) {
+            function next() {
+                if (index < migrationCommands.length)
+                {
+                    let command = migrationCommands[index];
+                    console.log("[#"+index+"] execute: " + command.fn);
+                    index++;
+                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                }
+                else
+                    resolve();
+            }
+            next();
+        });
+    },
+    info: info
+};
